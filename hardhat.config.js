45e20b8a22a43c30b7c983dc452f82d69bdc8165
miki710 <brown.opium.sugar@gmail.com>
2024-07-16 19:09:49 +0900

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition"); // 追加
+require("@nomiclabs/hardhat-ethers"); // 追加
+require("dotenv").config(); // dotenvを読み込む
+
+// 環境変数の値をログ出力
+console.log("ALCHEMY_MUMBAI_URL:", process.env.ALCHEMY_MUMBAI_URL);
+console.log("TEST_WALLET_PRIVATE_KEY:", process.env.TEST_WALLET_PRIVATE_KEY);
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  paths: {
+    sources: "./contracts", // コントラクトのソースディレクトリを指定
+    artifacts: "./artifacts", // アーティファクトのディレクトリを指定
+    cache: "./cache",
+    tests: "./test"
+  },
+  ignition: {
+    // 必要に応じて設定を追加
+  },
+  networks: {
+    amoy: {
+      url: process.env.ALCHEMY_MUMBAI_URL || "", // 環境変数を使用
+      accounts: [process.env.TEST_WALLET_PRIVATE_KEY], // 環境変数を使用
+    },
+  }
+};
